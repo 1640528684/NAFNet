f36b23f3b3a90314eaada5c522464cab07aa1c63
马少杰 <18765732312@163.com>
2025-03-31 12:26:10 +0800

    first

---
+++ basicsr/models/archs/v51_edit_arch.py
@@ -0,0 +1,324 @@
+# ------------------------------------------------------------------------
+# Copyright (c) 2022 megvii-model. All Rights Reserved.
+# ------------------------------------------------------------------------
+
+'''
+Simple Baselines for Image Restoration
+
+@article{chen2022simple,
+  title={Simple Baselines for Image Restoration},
+  author={Chen, Liangyu and Chu, Xiaojie and Zhang, Xiangyu and Sun, Jian},
+  journal={arXiv preprint arXiv:2204.04676},
+  year={2022}
+}
+'''
+import numbers
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from basicsr.models.archs.arch_util import LayerNorm2d
+from basicsr.models.archs.local_arch import Local_Base
+from einops import rearrange
+from torchvision import models
+import os
+import sys
+
+# 获取当前脚本所在目录
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 将该目录添加到 sys.path 中
+sys.path.append(current_dir)
+from layers import MultiSpectralAttentionLayer, SimpleGate2
+
+# 简单门控模块
+class SimpleGate(nn.Module):
+    def __init__(self, dim):
+        super(SimpleGate, self).__init__()
+
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        return x1 * x2
+
+# 双频前馈网络
+class DFFN(nn.Module):
+    def __init__(self, dim, ffn_expansion_factor=3, bias=False):
+        super(DFFN, self).__init__()
+        hidden_features = int(dim * ffn_expansion_factor)
+        self.project_in = nn.Conv2d(dim, hidden_features * 2, kernel_size=1, bias=bias)
+        self.dwconv = nn.Conv2d(hidden_features * 2, hidden_features * 2, kernel_size=3, stride=1, padding=1, groups=hidden_features * 2, bias=bias)
+        self.project_out = nn.Conv2d(hidden_features, dim, kernel_size=1, bias=bias)
+        self.sg = SimpleGate2()
+
+    def forward(self, x):
+        x = self.project_in(x)
+        x = self.dwconv(x)
+        x = self.sg(x)
+        x = self.project_out(x)
+        return x
+
+# 无偏层归一化
+class BiasFree_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape):
+        super(BiasFree_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        normalized_shape = torch.Size(normalized_shape)
+        assert len(normalized_shape) == 1
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+
+    def forward(self, x):
+        # 对通道维度进行归一化
+        mu = x.mean(dim=1, keepdim=True)
+        sigma = x.var(dim=1, keepdim=True, unbiased=False)
+        return (x - mu) / torch.sqrt(sigma + 1e-5) * self.weight
+
+# 有偏层归一化
+class WithBias_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape):
+        super(WithBias_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        normalized_shape = torch.Size(normalized_shape)
+        assert len(normalized_shape) == 1
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+
+    def forward(self, x):
+        # 对通道维度进行归一化
+        mu = x.mean(dim=1, keepdim=True)
+        sigma = x.var(dim=1, keepdim=True, unbiased=False)
+        # 确保 self.weight 和 self.bias 扩展到与 x 相同的维度
+        weight = self.weight.unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+        bias = self.bias.unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+        return (x - mu) / torch.sqrt(sigma + 1e-5) * weight + bias
+
+# 层归一化包装类
+class LayerNorm(nn.Module):
+    def __init__(self, dim, LayerNorm_type):
+        super(LayerNorm, self).__init__()
+        if LayerNorm_type == 'BiasFree':
+            self.body = BiasFree_LayerNorm(dim)
+        else:
+            self.body = WithBias_LayerNorm(dim)
+
+    def forward(self, x):
+        return self.body(x)
+
+
+# 缩放点积注意力
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, d_k, d_v, d_model):
+        super(ScaledDotProductAttention, self).__init__()
+        self.d_k = d_k
+        self.softmax = nn.Softmax(dim=-1)
+        self.fc = nn.Linear(d_v, d_model)
+
+    def forward(self, q, k, v, mask=None):
+        attn_scores = torch.matmul(q, k.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.d_k, dtype=torch.float32))
+        if mask is not None:
+            attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
+        attn_probs = self.softmax(attn_scores)
+        output = torch.matmul(attn_probs, v)
+        output = self.fc(output)
+        return output
+
+# 多头注意力
+class MultiHeadAttention(nn.Module):
+    def __init__(self, h, d_model, d_k, d_v):
+        super(MultiHeadAttention, self).__init__()
+        self.h = h
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.W_q = nn.Linear(d_model, h * d_k)
+        self.W_k = nn.Linear(d_model, h * d_k)
+        self.W_v = nn.Linear(d_model, h * d_v)
+        self.attention = ScaledDotProductAttention(d_k, d_v, d_model)
+        self.fc = nn.Linear(h * d_v, d_model)
+
+    def forward(self, q, k, v, mask=None):
+        batch_size = q.size(0)
+        Q = self.W_q(q).view(batch_size, -1, self.h, self.d_k).transpose(1, 2)
+        K = self.W_k(k).view(batch_size, -1, self.h, self.d_k).transpose(1, 2)
+        V = self.W_v(v).view(batch_size, -1, self.h, self.d_v).transpose(1, 2)
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        output = self.attention(Q, K, V, mask)
+        output = output.transpose(1, 2).contiguous().view(batch_size, -1, self.h * self.d_v)
+        output = self.fc(output)
+        return output
+
+# 变压器块
+class TransformerBlock(nn.Module):
+    def __init__(self, dim, ffn_expansion_factor=1, bias=False, LayerNorm_type='WithBias', att=False):
+        super(TransformerBlock, self).__init__()
+        self.norm1 = LayerNorm(dim, LayerNorm_type)
+        self.att = att
+        if att:
+            self.mha = MultiHeadAttention(h=8, d_model=dim, d_k=dim // 8, d_v=dim // 8)
+        self.norm2 = LayerNorm(dim, LayerNorm_type)
+        self.ffn = DFFN(dim, ffn_expansion_factor=ffn_expansion_factor, bias=bias)
+
+    def forward(self, x):
+        if self.att:
+            x = x + self.mha(self.norm1(x), self.norm1(x), self.norm1(x))
+        x = x + self.ffn(self.norm2(x))
+        return x
+
+# NAF块
+class NAFBlock(nn.Module):
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+        super(NAFBlock, self).__init__()
+        dw_channel = c * DW_Expand
+        self.conv1 = nn.Conv2d(c, dw_channel, 1, padding=0, stride=1, groups=1, bias=True)
+        self.conv2 = nn.Conv2d(dw_channel, dw_channel, 3, padding=1, stride=1, groups=dw_channel, bias=True)
+        self.conv3 = nn.Conv2d(dw_channel // 2, c, 1, padding=0, stride=1, groups=1, bias=True)
+        self.sg = SimpleGate(dim=c)  # 传递 dim 参数
+        ffn_channel = FFN_Expand * c
+        self.conv4 = nn.Conv2d(c, ffn_channel, 1, padding=0, stride=1, groups=1, bias=True)
+        self.conv5 = nn.Conv2d(ffn_channel // 2, c, 1, padding=0, stride=1, groups=1, bias=True)
+        self.norm1 = LayerNorm(c, LayerNorm_type='WithBias')
+        self.norm2 = LayerNorm(c, LayerNorm_type='WithBias')
+        self.dropout = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
+
+    def forward(self, inp):
+        x = inp
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.sg(x)
+        x = self.conv3(x)
+        x = self.dropout(x)
+        x = inp + x
+        y = self.norm1(x)
+        y = self.conv4(y)
+        y = self.sg(y)
+        y = self.conv5(y)
+        y = self.dropout(y)
+        return x + y
+
+# 特征金字塔网络
+class FPN(nn.Module):
+    def __init__(self, in_channels_list, out_channels=256):
+        super(FPN, self).__init__()
+        self.inner_blocks = nn.ModuleList()
+        self.layer_blocks = nn.ModuleList()
+        for in_channels in in_channels_list:
+            self.inner_blocks.append(nn.Conv2d(in_channels, out_channels, 1))
+            self.layer_blocks.append(nn.Conv2d(out_channels, out_channels, 3, padding=1))
+
+    def forward(self, x):
+        last_inner = self.inner_blocks[-1](x[-1])
+        results = []
+        results.append(self.layer_blocks[-1](last_inner))
+        for i in range(len(x) - 2, -1, -1):
+            inner_lateral = self.inner_blocks[i](x[i])
+            feat_shape = inner_lateral.shape[-2:]
+            last_inner = F.interpolate(last_inner, size=feat_shape, mode="nearest")
+            last_inner = last_inner + inner_lateral
+            results.insert(0, self.layer_blocks[i](last_inner))
+        return results
+
+# VGG感知损失
+class VGGLoss(nn.Module):
+    def __init__(self, vgg_model):
+        super(VGGLoss, self).__init__()
+        self.vgg = vgg_model
+        self.criterion = nn.MSELoss()
+
+    def forward(self, x, y):
+        x_features = self.extract_features(x)
+        y_features = self.extract_features(y)
+        loss = 0
+        for xf, yf in zip(x_features, y_features):
+            loss += self.criterion(xf, yf)
+        return loss
+
+    def extract_features(self, x):
+        features = []
+        for layer in self.vgg.features:
+            x = layer(x)
+            if isinstance(layer, nn.ReLU):
+                features.append(x)
+        return features
+
+# NAFNet主类
+class NAFNet(nn.Module):
+
+    def __init__(self, img_channel=3, width=16, middle_blk_num=1, enc_blk_nums=[], dec_blk_nums=[]):
+        super(NAFNet, self).__init__()
+        self.intro = nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
+                               bias=True)
+        self.ending = nn.Conv2d(in_channels=width, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1,
+                                bias=True)
+        self.encoders = nn.ModuleList()
+        self.decoders = nn.ModuleList()
+        self.middle_blks = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        chan = width
+        for num in enc_blk_nums:
+            self.encoders.append(
+                nn.Sequential(
+                    *[NAFBlock(chan) for _ in range(num)]
+                )
+            )
+            self.downs.append(
+                nn.Conv2d(chan, 2 * chan, 2, 2)
+            )
+            chan = chan * 2
+        self.middle_blks = \
+            nn.Sequential(
+                *[NAFBlock(chan) for _ in range(middle_blk_num)]
+            )
+        for num in dec_blk_nums:
+            self.ups.append(
+                nn.Sequential(
+                    nn.Conv2d(chan, chan * 2, 1),
+                    nn.PixelShuffle(2)
+                )
+            )
+            chan = chan // 2
+            self.decoders.append(
+                nn.Sequential(
+                    *[NAFBlock(chan) for _ in range(num)]
+                )
+            )
+        self.padder_size = 2 ** len(self.encoders)
+
+    def forward(self, inp):
+        B, C, H, W = inp.shape
+        inp = self.check_image_size(inp)
+        x = self.intro(inp)
+        encs = []
+        for encoder, down in zip(self.encoders, self.downs):
+            x = encoder(x)
+            encs.append(x)
+            x = down(x)
+        x = self.middle_blks(x)
+        for decoder, up, enc_skip in zip(self.decoders, self.ups, reversed(encs)):
+            x = up(x)
+            x = x + enc_skip
+            x = decoder(x)
+        x = self.ending(x)
+        x = x[:, :, :H, :W]
+        return x
+
+    def check_image_size(self, x):
+        _, _, h, w = x.size()
+        mod_pad_h = (self.padder_size - h % self.padder_size) % self.padder_size
+        mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
+        x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
+        return x
+
+    def self_supervised_forward(self, inp):
+        """自监督学习的前向传播"""
+        return self.forward(inp)
+
+
+# 本地编辑类
+class v51editLocal(Local_Base, NAFNet):
+    def __init__(self, *args, train_size=(1, 3, 256, 256), fast_imp=False, **kwargs):
+        super(v51editLocal, self).__init__(*args, **kwargs)
+        self.train_size = train_size
+        self.fast_imp = fast_imp
+        base_size = (train_size[2], train_size[3])
+        self.convert(base_size=base_size, train_size=train_size, fast_imp=fast_imp)
---
+++ basicsr/train.py
@@ -0,0 +1,368 @@
+# ------------------------------------------------------------------------
+# Copyright (c) 2022 megvii-model. All Rights Reserved.
+# ------------------------------------------------------------------------
+# Modified from BasicSR (https://github.com/xinntao/BasicSR)
+# Copyright 2018-2020 BasicSR Authors
+# ------------------------------------------------------------------------
+import argparse
+import datetime
+import logging
+import math
+import random
+import time
+import torch
+from os import path as osp
+
+import os
+import sys
+# 获取 basicsr 所在目录
+# sys.path.append('E:\\NAFNet_copy')
+# 动态获取当前脚本所在目录
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_dir = os.path.dirname(current_dir)  # 获取项目根目录
+sys.path.append(project_dir)  # 添加项目根目录到 sys.path
+
+from basicsr.data import create_dataloader, create_dataset
+from basicsr.data.data_sampler import EnlargedSampler
+from basicsr.data.prefetch_dataloader import CPUPrefetcher, CUDAPrefetcher
+from basicsr.models import create_model
+from basicsr.utils import (MessageLogger, check_resume, get_env_info,
+                           get_root_logger, get_time_str, init_tb_logger,
+                           init_wandb_logger, make_exp_dirs, mkdir_and_rename,
+                           set_random_seed)
+from basicsr.utils.dist_util import get_dist_info, init_dist
+from basicsr.utils.options import dict2str, parse
+
+
+def parse_options(is_train=True):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-opt', type=str, required=True, help='Path to option YAML file.')
+    parser.add_argument(
+        '--launcher',
+        choices=['none', 'pytorch', 'slurm'],
+        default='none',
+        help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+
+    parser.add_argument('--input_path', type=str, required=False, help='The path to the input image. For single image inference only.')
+    parser.add_argument('--output_path', type=str, required=False, help='The path to the output image. For single image inference only.')
+
+    args = parser.parse_args()
+    opt = parse(args.opt, is_train=is_train)
+
+    # distributed settings
+    if args.launcher == 'none':
+        opt['dist'] = False
+        print('Disable distributed.', flush=True)
+    else:
+        opt['dist'] = True
+        if args.launcher == 'slurm' and 'dist_params' in opt:
+            init_dist(args.launcher, **opt['dist_params'])
+        else:
+            init_dist(args.launcher)
+            print('init dist .. ', args.launcher)
+
+    opt['rank'], opt['world_size'] = get_dist_info()
+
+    # random seed
+    seed = opt.get('manual_seed')
+    if seed is None:
+        seed = random.randint(1, 10000)
+        opt['manual_seed'] = seed
+    set_random_seed(seed + opt['rank'])
+
+    if args.input_path is not None and args.output_path is not None:
+        opt['img_path'] = {
+            'input_img': args.input_path,
+            'output_img': args.output_path
+        }
+
+    # 设置设备为 CPU
+    # opt['device'] = 'cpu'
+    # opt['num_gpu'] = 0
+    opt['num_gpu'] = torch.cuda.device_count() if torch.cuda.is_available() else 0
+    if opt['num_gpu'] > 0:
+        opt['device'] = 'cuda'
+    else:
+        opt['device'] = 'cpu'
+
+    return opt
+
+
+def init_loggers(opt):
+    log_file = osp.join(opt['path']['log'],
+                        f"train_{opt['name']}_{get_time_str()}.log")
+    logger = get_root_logger(
+        logger_name='basicsr', log_level=logging.INFO, log_file=log_file)
+    logger.info(get_env_info())
+    logger.info(dict2str(opt))
+
+    # initialize wandb logger before tensorboard logger to allow proper sync:
+    if (opt['logger'].get('wandb')
+            is not None) and (opt['logger']['wandb'].get('project')
+                              is not None) and ('debug' not in opt['name']):
+        assert opt['logger'].get('use_tb_logger') is True, (
+            'should turn on tensorboard when using wandb')
+        init_wandb_logger(opt)
+    tb_logger = None
+    if opt['logger'].get('use_tb_logger') and 'debug' not in opt['name']:
+        # tb_logger = init_tb_logger(log_dir=f'./logs/{opt['name']}') #mkdir logs @CLY
+        tb_logger = init_tb_logger(log_dir=osp.join('logs', opt['name']))
+    return logger, tb_logger
+
+
+def create_train_val_dataloader(opt, logger):
+    # create train and val dataloaders
+    train_loader, val_loader = None, None
+    for phase, dataset_opt in opt['datasets'].items():
+        if phase == 'train':
+            dataset_enlarge_ratio = dataset_opt.get('dataset_enlarge_ratio', 1)
+            train_set = create_dataset(dataset_opt)
+            train_sampler = EnlargedSampler(train_set, opt['world_size'],
+                                            opt['rank'], dataset_enlarge_ratio)
+            train_loader = create_dataloader(
+                train_set,
+                dataset_opt,
+                num_gpu=opt['num_gpu'],
+                dist=opt['dist'],
+                sampler=train_sampler,
+                seed=opt['manual_seed'])
+
+            num_iter_per_epoch = math.ceil(
+                len(train_set) * dataset_enlarge_ratio /
+                (dataset_opt['batch_size_per_gpu'] * opt['world_size']))
+            total_iters = int(opt['train']['total_iter'])
+            total_epochs = math.ceil(total_iters / (num_iter_per_epoch))
+            logger.info(
+                'Training statistics:'
+                f'\n\tNumber of train images: {len(train_set)}'
+                f'\n\tDataset enlarge ratio: {dataset_enlarge_ratio}'
+                f'\n\tBatch size per gpu: {dataset_opt["batch_size_per_gpu"]}'
+                f'\n\tWorld size (gpu number): {opt["world_size"]}'
+                f'\n\tRequire iter number per epoch: {num_iter_per_epoch}'
+                f'\n\tTotal epochs: {total_epochs}; iters: {total_iters}.')
+
+        elif phase == 'val':
+            val_set = create_dataset(dataset_opt)
+            val_loader = create_dataloader(
+                val_set,
+                dataset_opt,
+                num_gpu=opt['num_gpu'],
+                dist=opt['dist'],
+                sampler=None,
+                seed=opt['manual_seed'])
+            logger.info(
+                f'Number of val images/folders in {dataset_opt["name"]}: '
+                f'{len(val_set)}')
+        else:
+            raise ValueError(f'Dataset phase {phase} is not recognized.')
+
+    return train_loader, train_sampler, val_loader, total_epochs, total_iters
+
+
+def main():
+    # parse options, set distributed setting, set ramdom seed
+    opt = parse_options(is_train=True)
+
+    # distributed settings
+    if opt['dist']:
+        init_dist(opt['launcher'], **opt['dist_params'])
+    else:
+        opt['rank'], opt['world_size'] = 0, 1
+
+
+    #torch.backends.cudnn.benchmark = True
+    # torch.backends.cudnn.deterministic = True
+
+    # automatic resume ..
+    # state_folder_path = 'experiments/{}/training_states/'.format(opt['name'])
+    #修改点2
+    state_folder_path = os.path.join(current_dir, 'experiments', opt['name'], 'training_states')  # 修改为动态路径
+    # import os
+    # try:
+    #     states = os.listdir(state_folder_path)
+    # except:
+    #     states = []
+    try:
+        states = os.listdir(state_folder_path)
+    except:
+        states = []
+
+    resume_state = None
+    if len(states) > 0:
+        print('!!!!!! resume state .. ', states, state_folder_path)
+        max_state_file = '{}.state'.format(max([int(x[0:-6]) for x in states]))
+        resume_state = os.path.join(state_folder_path, max_state_file)
+        opt['path']['resume_state'] = resume_state
+
+    # load resume states if necessary
+    if opt['path'].get('resume_state'):
+        # 从 CPU 加载模型
+        resume_state = torch.load(opt['path']['resume_state'], map_location=torch.device('cpu'))
+    else:
+        resume_state = None
+
+    # mkdir for experiments and logger
+    if resume_state is None:
+        make_exp_dirs(opt)
+        if opt['logger'].get('use_tb_logger') and 'debug' not in opt[
+                'name'] and opt['rank'] == 0:
+            mkdir_and_rename(osp.join('tb_logger', opt['name']))
+
+    # initialize loggers
+    logger, tb_logger = init_loggers(opt)
+
+    # create train and validation dataloaders
+    result = create_train_val_dataloader(opt, logger)
+    train_loader, train_sampler, val_loader, total_epochs, total_iters = result
+
+    # create model
+    if resume_state:  # resume training
+        check_resume(opt, resume_state['iter'])
+        model = create_model(opt)
+        if opt['num_gpu'] > 1:
+            model = torch.nn.DataParallel(model)
+        model.resume_training(resume_state)  # handle optimizers and schedulers
+        logger.info(f"Resuming training from epoch: {resume_state['epoch']}, "
+                    f"iter: {resume_state['iter']}.")
+        start_epoch = resume_state['epoch']
+        current_iter = resume_state['iter']
+    else:
+        model = create_model(opt)
+        if opt['num_gpu'] > 1:
+            model = torch.nn.DataParallel(model)
+        start_epoch = 0
+        current_iter = 0
+
+    # create message logger (formatted outputs)
+    msg_logger = MessageLogger(opt, current_iter, tb_logger)
+
+    # dataloader prefetcher
+    prefetch_mode = opt['datasets']['train'].get('prefetch_mode')
+    if prefetch_mode is None or prefetch_mode == 'cpu':
+        prefetcher = CPUPrefetcher(train_loader)
+    elif prefetch_mode == 'cuda':
+        # 由于使用 CPU，强制使用 CPU 预取
+        #prefetch_mode = 'cpu'
+        #prefetcher = CPUPrefetcher(train_loader)
+        prefetcher = CUDAPrefetcher(train_loader, opt)
+        logger.info(f'Use {prefetch_mode} prefetch dataloader')
+        if opt['datasets']['train'].get('pin_memory') is not True:
+            raise ValueError('Please set pin_memory=True for CUDAPrefetcher.')
+    else:
+        raise ValueError(f'Wrong prefetch_mode {prefetch_mode}.'
+                         "Supported ones are: None, 'cuda', 'cpu'.")
+
+    # training
+    logger.info(
+        f'Start training from epoch: {start_epoch}, iter: {current_iter}')
+    data_time, iter_time = time.time(), time.time()
+    start_time = time.time()
+    best_psnr = -36.9
+
+    # 计算每个 epoch 的迭代次数和总 epoch 数
+    num_iter_per_epoch = math.ceil(
+        len(train_loader.dataset) * opt['datasets']['train'].get('dataset_enlarge_ratio', 1) /
+        (opt['datasets']['train']['batch_size_per_gpu'] * opt['world_size']))
+    total_epochs = math.ceil(total_iters / num_iter_per_epoch)
+
+    # 训练循环
+    for epoch in range(start_epoch, total_epochs):
+        train_sampler.set_epoch(epoch)
+        prefetcher.reset()
+        train_data = prefetcher.next()
+
+        while train_data is not None:
+            data_time = time.time() - data_time
+
+            current_iter += 1
+            if current_iter > total_iters:
+                break
+            # update learning rate
+            model.update_learning_rate(
+                current_iter, warmup_iter=opt['train'].get('warmup_iter', -1))
+            # training
+            # 将数据移动到 CPU
+            # for key in train_data:
+            #     if isinstance(train_data[key], torch.Tensor):
+            #         train_data[key] = train_data[key].to(torch.device('cpu'))
+            #     elif isinstance(train_data[key], list):
+            #         train_data[key] = [item.to(torch.device('cpu')) if isinstance(item, torch.Tensor) else item for item in train_data[key]]
+            # 根据设备设置（CPU 或 GPU）移动数据
+            device = torch.device(opt['device'])
+            for key in train_data:
+                if isinstance(train_data[key], torch.Tensor):
+                    train_data[key] = train_data[key].to(device)
+                elif isinstance(train_data[key], list):
+                    train_data[key] = [item.to(device) if isinstance(item, torch.Tensor) else item for item in train_data[key]]
+            model.feed_data(train_data, is_val=False)
+
+            result_code = model.optimize_parameters(current_iter, tb_logger)
+            iter_time = time.time() - iter_time
+            # log
+            if current_iter % opt['logger']['print_freq'] == 0:
+                log_vars = {'epoch': epoch, 'iter': current_iter, 'total_iter': total_iters}
+                log_vars.update({'lrs': model.get_current_learning_rate()})
+                log_vars.update({'time': iter_time, 'data_time': data_time})
+                log_vars.update(model.get_current_log())
+                msg_logger(log_vars)
+            # 保存最好的loss
+            if current_iter % opt['logger']['save_checkpoint_freq'] == 0:
+                logger.info('Saving models and training states.')
+                model.save(epoch, current_iter)
+
+            # validation
+            if opt.get('val') is not None and (current_iter % opt['val']['val_freq'] == 0):
+                rgb2bgr = opt['val'].get('rgb2bgr', True)
+                use_image = opt['val'].get('use_image', True)
+                # 将验证数据移动到 CPU
+                # for i in range(len(val_loader.dataset)):
+                #     for key in val_loader.dataset[i]:
+                #         if isinstance(val_loader.dataset[i][key], torch.Tensor):
+                #             val_loader.dataset[i][key] = val_loader.dataset[i][key].to(torch.device('cpu'))
+                #         elif isinstance(val_loader.dataset[i][key], list):
+                #             val_loader.dataset[i][key] = [item.to(torch.device('cpu')) if isinstance(item, torch.Tensor) else item for item in val_loader.dataset[i][key]]
+
+                # model.validation(val_loader, current_iter, tb_logger,
+                #              opt['val']['save_img'], rgb2bgr, use_image)
+                log_vars = {'epoch': epoch, 'iter': current_iter, 'total_iter': total_iters}
+                log_vars.update({'lrs': model.get_current_learning_rate()})
+                log_vars.update(model.get_current_log())
+                msg_logger(log_vars)
+
+            data_time = time.time()
+            iter_time = time.time()
+            train_data = prefetcher.next()
+
+        # end of iter
+        logger.info(f'End of epoch {epoch + 1}/{total_epochs}.')
+        epoch += 1
+
+    # end of epoch
+
+    consumed_time = str(
+        datetime.timedelta(seconds=int(time.time() - start_time)))
+    logger.info(f'End of training. Time consumed: {consumed_time}')
+    logger.info('Save the latest model.')
+    model.save(epoch=-1, current_iter=-1)  # -1 stands for the latest
+    if opt.get('val') is not None:
+        rgb2bgr = opt['val'].get('rgb2bgr', True)
+        use_image = opt['val'].get('use_image', True)
+        # 将验证数据移动到 CPU
+        # for i in range(len(val_loader.dataset)):
+        #     for key in val_loader.dataset[i]:
+        #         if isinstance(val_loader.dataset[i][key], torch.Tensor):
+        #             val_loader.dataset[i][key] = val_loader.dataset[i][key].to(torch.device('cpu'))
+        #         elif isinstance(val_loader.dataset[i][key], list):
+        #             val_loader.dataset[i][key] = [item.to(torch.device('cpu')) if isinstance(item, torch.Tensor) else item for item in val_loader.dataset[i][key]]  
+        metric = model.validation(val_loader, current_iter, tb_logger,
+                                  opt['val']['save_img'], rgb2bgr, use_image)
+    if tb_logger:
+        tb_logger.close()
+
+
+if __name__ == '__main__':
+    import os
+    os.environ['GRPC_POLL_STRATEGY'] = 'epoll1'
+    main()
